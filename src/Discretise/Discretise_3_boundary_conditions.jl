a63650646906b8b43415ac2aa7df08cf2801252b
Joshua Leyland <egyjl6@nottingham.ac.uk>
2025-04-23 12:13:01 +0100

    Added comment for creating new dirichlet discretisation

--- src/Discretise/Discretise_3_boundary_conditions.jl
+++ src/Discretise/Discretise_3_boundary_conditions.jl
@@ -33,7 +33,8 @@ end
     # 0.0, 0.0
 end
 
-# KWallFunction JL: copy the structure of these for my two WFs
+# KWallFunction 
+#= JL: copy the structure of these for my two WFs, do this in the dirichletFunction.jl file =#
 @inline (bc::KWallFunction)(
     term::Operator{F,P,I,Divergence{Linear}}, colval, rowptr, nzval, cellID, zcellID, cell, face, fID, i, component, time) where {F,P,I} = begin
     flux = term.flux[fID]
