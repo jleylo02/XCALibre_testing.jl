926a1a566134bb5b685638346819a2207a0f5467
Joshua Leyland <egyjl6@nottingham.ac.uk>
2025-04-24 09:55:49 +0100

    neumannFunction_interpolation populated

--- src/Discretise/boundary_conditions/neumannFunction_interpolation.jl
+++ src/Discretise/boundary_conditions/neumannFunction_interpolation.jl
@@ -1,19 +1,66 @@
-
-const NEUMANN = Union{Neumann, KWallFunction, NutWallFunction, OmegaWallFunction}
-
-function adjust_boundary!(b_cpu, BC::NEUMANN, phif::FaceScalarField, phi, boundaries, boundary_cellsID, time, backend, workgroup)
+# Implementation to dispatch when user provides an simple function
+function adjust_boundary!(b_cpu, BC::NeumannFunction{T,Test}, phif::FaceScalarField, phi, boundaries, boundary_cellsID, time, backend, workgroup
+    ) where {T,Test<:Function}
     phif_values = phif.values
     phi_values = phi.values
 
     kernel_range = length(b_cpu[BC.ID].IDs_range)
 
-    kernel! = adjust_boundary_neumann_scalar!(backend, workgroup)
+    kernel! = adjust_boundary_neumannFunction_scalar!(backend, workgroup)
     kernel!(BC, phif, phi, boundaries, boundary_cellsID, time, phif_values, phi_values, ndrange = kernel_range)
     # # KernelAbstractions.synchronize(backend)
 end
 
-# Neumann
-@kernel function adjust_boundary_neumann_scalar!(BC, phif, phi, boundaries, boundary_cellsID, time, phif_values, phi_values)
+function adjust_boundary!(b_cpu, BC::NeumannFunction{T,Test}, psif::FaceVectorField, psi::VectorField, boundaries, boundary_cellsID, time, backend, workgroup
+    ) where {T,Test<:Function}
+    (; x, y, z) = psif
+
+    kernel_range = length(b_cpu[BC.ID].IDs_range)
+
+    kernel! = adjust_boundary_neumann_vector!(backend, workgroup)
+    kernel!(BC, psif, psi, boundaries, boundary_cellsID, time, x, y, z, ndrange = kernel_range)
+    # # KernelAbstractions.synchronize(backend)
+end
+
+# Implementation to dispatch when user provides an XCALibreUserFunctor
+function adjust_boundary!(b_cpu, BC::NeumannFunction{T,Test}, phif::FaceScalarField, phi, boundaries, boundary_cellsID, time, backend, workgroup
+    ) where {T,Test<:XCALibreUserFunctor}
+    phif_values = phif.values
+    phi_values = phi.values
+
+    kernel_range = length(b_cpu[BC.ID].IDs_range)
+
+    if !BC.value.steady
+        config = (;hardware=(;backend=backend, workgroup=workgroup)) # temp solution
+        update_user_boundary!(
+            BC, eqnModel, component, faces, cells, facesID_range, time, config)
+    end
+
+    kernel! = adjust_boundary_neumannFunction_scalar!(backend, workgroup)
+    kernel!(BC, phif, phi, boundaries, boundary_cellsID, time, phif_values, phi_values, ndrange = kernel_range)
+    # # KernelAbstractions.synchronize(backend)
+end
+
+function adjust_boundary!(b_cpu, BC::NeumannFunction{T,Test}, psif::FaceVectorField, psi::VectorField, boundaries, boundary_cellsID, time, backend, workgroup
+    ) where {T,Test<:XCALibreUserFunctor}
+    (; x, y, z) = psif
+
+    kernel_range = length(b_cpu[BC.ID].IDs_range)
+
+    if !BC.value.steady
+        config = (;hardware=(;backend=backend, workgroup=workgroup)) # temp solution
+        update_user_boundary!(
+            BC, eqnModel, component, faces, cells, facesID_range, time, config)
+    end
+
+    kernel! = adjust_boundary_neumann_vector!(backend, workgroup)
+    kernel!(BC, psif, psi, boundaries, boundary_cellsID, time, x, y, z, ndrange = kernel_range)
+    # # KernelAbstractions.synchronize(backend)
+end
+
+# Implement interpolation for scalars and vectors
+
+@kernel function adjust_boundary_neumannFunction_scalar!(BC, phif, phi, boundaries, boundary_cellsID, time, phif_values, phi_values)
     i = @index(Global)
 
     @inbounds begin
@@ -24,26 +71,8 @@ end
     end
 end
 
-function adjust_boundary!(b_cpu, BC::NEUMANN, psif::FaceVectorField, psi::VectorField, boundaries, boundary_cellsID, time, backend, workgroup)
-    (; x, y, z) = psif
-
-    kernel_range = length(b_cpu[BC.ID].IDs_range)
-
-    kernel! = adjust_boundary_neumann_vector!(backend, workgroup)
-    kernel!(BC, psif, psi, boundaries, boundary_cellsID, time, x, y, z, ndrange = kernel_range)
-    # # KernelAbstractions.synchronize(backend)
-end
-
-@kernel function adjust_boundary_neumann_vector!(BC, psif, psi, boundaries, boundary_cellsID, time, x, y, z)
+@kernel function adjust_boundary_neumannFunction_vector!(BC, psif, psi, boundaries, boundary_cellsID, time, x, y, z)
     i = @index(Global)
 
     @inbounds begin
-        (; IDs_range) = boundaries[BC.ID]
-        fID = IDs_range[i]
-        cID = boundary_cellsID[fID]
-        psi_cell = psi[cID]
-        x[fID] = psi_cell[1]
-        y[fID] = psi_cell[2]
-        z[fID] = psi_cell[3]
-    end
-end
+        (; I
