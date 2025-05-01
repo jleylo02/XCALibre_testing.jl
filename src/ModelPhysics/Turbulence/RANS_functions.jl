550efa7a94590c955555a912742fb0e4f9d2f235
Joshua Leyland <egyjl6@nottingham.ac.uk>
2025-05-01 12:05:06 +0100

    creates correct_nut_wall! logic for NN

--- src/ModelPhysics/Turbulence/RANS_functions.jl
+++ src/ModelPhysics/Turbulence/RANS_functions.jl
@@ -319,6 +319,8 @@ function set_production!(P, BC::NeumannFunction, model, gradU, config)
     facesID_range = boundaries_cpu[BC.ID].IDs_range
     start_ID = facesID_range[1]
 
+    (; output, input, network, gradient) = BC.value
+    
     # Execute apply boundary conditions kernel
     kernel! = _set_production_NN!(backend, workgroup)
     kernel!(
@@ -412,4 +414,51 @@ end
     else
         values[fID] = 0.0
     end
+end
+
+function correct_nut_wall!(νtf, BC::NeumannFunction, model, config)
+    # backend = _get_backend(mesh)
+    (; hardware) = config
+    (; backend, workgroup) = hardware
+    
+    # Deconstruct mesh to required fields
+    mesh = model.domain
+    (; faces, boundary_cellsID, boundaries) = mesh
+
+    # Extract physics models
+    (; fluid, turbulence) = model
+
+    # facesID_range = get_boundaries(BC, boundaries)
+    boundaries_cpu = get_boundaries(boundaries)
+    facesID_range = boundaries_cpu[BC.ID].IDs_range
+    start_ID = facesID_range[1]
+
+    (; output, input, network) = BC.value
+
+    # Execute apply boundary conditions kernel
+    kernel! = _correct_nut_wall_NN!(backend, workgroup)
+    kernel!(
+        νtf.values, fluid, turbulence, BC, faces, boundary_cellsID, start_ID, ndrange=length(facesID_range)
+    )
+end
+
+@kernel function _correct_nut_wall_NN!(
+    values, fluid, turbulence, BC, faces, boundary_cellsID, start_ID)
+i = @index(Global)
+    fID = i + start_ID - 1 # Redefine thread index to become face ID
+    
+    (; input, output) = BC.value 
+    (; nu) = fluid
+    (; k, nutf) = turbulence
+    
+    cID = boundary_cellsID[fID]
+    face = faces[fID]
+    nuc = nu[cID]
+    (; delta, normal)= face
+    cmu = 0.09
+    yplus = y_plus(k[cID], nuc, delta, cmu)
+    input = (yplus .- data_mean) ./ data_std
+        
+    nutw = nuc.*(input./output)
+    values[fID] = nutw
 end
